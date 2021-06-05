e12cf47f642999151d033fc7338478ba742d9a3e
Manish Jadli <mnish.jhadli@irasys.biz>
2021-06-05 09:18:53 +0530

    https://github.com/manish-jadli/mj-blog.git

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import homeImage from'./../images/home.jpeg';
 
 export default function Home(){
     return(
-        <h1>Home page</h1>
+        <main>
+            <img src={homeImage} alt="Home Image" className="absolute object-cover w-full h-full" />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-slug home-name">Aloha. I'm Manish Jadli.</h1>
+            </section>
+        </main>
     )
 }
