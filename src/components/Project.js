53e88c8cd0319700c1a53cbf1af664a441b2bb38
Manish Jadli <mnish.jhadli@irasys.biz>
2021-06-14 21:31:13 +0530

    add project screen with sanity data

--- src/components/Project.js
+++ src/components/Project.js
@@ -28,7 +28,7 @@ export default function Project(){
                     <article className="relative rounded-lg shadow-xl bg-white p-16">
                         <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700"><a href={project.link} alt={project.title} target="_blank" rel="noopener noreferrer">{project.title}</a></h3>
                         <div className="text-gray-500 text-xs space-x-4">
-                            <span><strong className="font-bold">Finished on</strong>:{" "} {new Data(project.date).toLocaleDateString()}</span>
+                            <span><strong className="font-bold">Finished on</strong>:{" "} {new Date(project.date).toLocaleDateString()}</span>
                             <span><strong className="font-bold">Company</strong>:{" "} {project.place}</span>
                             <span><strong className="font-bold">Type</strong>{" "} {project.projectType}</span>
                             <p className="my-6 text-lg text-gray-700 leading-relaxed">{project.description}</p>
