c7c9505e383780d053dd47a578f27639a1733c4e
Manish Jadli <mnish.jhadli@irasys.biz>
2021-06-16 18:26:49 +0530

    update howe & singlepost screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home(){
         <main>
             <img src={homeImage} className="absolute object-cover w-full h-full" />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-slug home-name">Aloha. I'm Manish Jadli.</h1>
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-slug home-name">Welcome in mj-book website.</h1>
             </section>
         </main>
     )
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import sanityClient from '../client.js';
+import { imageUrlBuilder } from '@sanity/image-url';
+
+const builder= imageUrlBuilder(sanityClient);
+function urlFor(source){
+    return builder.image(source)
+}
+
 
 export default function SinglePost(){
+
+    const [singlePost, setSinglePost]= useState(null);
+    const {slug} = useParams();
+
+    useEffect(()=>{
+        sanityClient.fetch(`*[slug.current == "${slug}"]{
+            title,
+            _id,
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                }
+            },
+            body,
+            "name":author->name,
+            "authorImage":author->image
+        }`).then((data)=>setSinglePost(data[0]))
+        .catch(console.error);
+    },[slug]);
+
+    if(!singlePost) return <div>Loading...</div>
+
     return(
-        <h1>SinglePost page</h1>
+        <main className="bg-gray-200 min-h-screen p-12">
+            <article className="container mx-auto bg-green-100 rounded-lg">
+                <header className="relative">
+                    <div className="absolute h-full w-full flex items-center justify-center p-8">
+                        <div className="bg-white bg-opacity-75 rounded p-12">
+                            <h1 className="cursive text-3xl lg:text-6xl mb-4">{singlePost.title}</h1>
+                            <div className="flex justify-center text-gray-800">
+                                <img src={urlFor(singlePost.authorImage).url()} alt={singlePost.name} className="w-10 h-10 rounded-full" />
+                                <p className="cursive flex items-center pl-2 text-2xl">{singlePost.name}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img />
+                </header>
+                <div>Block Content</div>
+            </article>
+        </main>
     )
 }
