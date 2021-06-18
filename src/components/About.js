999fbfe6e8dc379e60ef628e7d5215b55d81a6ff
Manish Jadli <mnish.jhadli@irasys.biz>
2021-06-18 09:57:06 +0530

    aobut us screen

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import sanityClient  from '../client.js';
+import plumeria from '../images/home.jpeg';
+import imageUrlBuilder from '@sanity/image-url';
+import BlockContent from '@sanity/block-content-to-react';
+
+const builder=imageUrlBuilder(sanityClient);
+function urlFor(source){
+    return builder.image(source);
+}
 
 export default function About(){
+
+    const [author, setAuthor]=useState(null);
+    useEffect(()=>{
+        sanityClient.fetch(`*[_type=="author"]{
+            name,
+            bio,
+            "authorImage":image.asset->url
+        }`).then((data)=>setAuthor(data[0]))
+        .catch(console.error);
+    },[]);
+
+    if(!author) return <div>Loading...</div>;
+
     return(
-        <h1>About page</h1>
-    )
+        <main className="relative">
+            <img src={plumeria} alt="Plumeria Flower" className="absolute w-full" />
+            <div className="p-10 lg:pt-48 container mx-auto relative">
+                <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+                    <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt="Author" />
+                    <div className="text-lg flex flex-col justify-center">
+                        <h1 className="cursive text-6xl text-green-300 mb-4">Hey there. I'M <span className="text-green-100">{author.name}</span></h1>
+                        <div className="prose lg:prose-xl text-white">
+                            <BlockContent blocks={author.bio} projectId="f8j7mwsm" dataset="production" />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    );
 }
