f396893fe804b392ae4a775c2dd6b99915428971
Manish Jadli <mnish.jhadli@irasys.biz>
2021-06-20 07:59:58 +0530

    resolved project and refresh page issue

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,7 +6,7 @@ export default function Project(){
     const [projectData, setProjectData]=useState(null);
 
     useEffect(()=>{
-        sanityClient.fetch(`*[_title=="project"]{
+        sanityClient.fetch(`*[_type=="project"]{
             title,
             date,
             place,
@@ -22,7 +22,7 @@ export default function Project(){
        <main className="bg-green-100 min-h-screen p-12">
            <section className="container mx-auto">
                 <h1 className="text-5xl flex justify-center cursive">My Projects</h1>
-                <h2 className="text-lg text-gray-600 flex justify-cener mb-12">Welcome to my projects page!</h2>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my projects page!</h2>
                 <section className="grid grid-cols-2 gap-8">
                     {projectData && projectData.map((project, index)=>(
                     <article className="relative rounded-lg shadow-xl bg-white p-16">
