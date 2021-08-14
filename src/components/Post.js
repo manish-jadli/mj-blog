7d10acbe75b7e83c18b0d0eebfbaa93783d962f9
manish jadli <manishjadli@manishs-MacBook-Pro.local>
2021-08-14 19:56:43 +0530

    UI Change in blog screen

--- src/components/Post.js
+++ src/components/Post.js
@@ -34,14 +34,14 @@ export default function Post(){
             <section className="container mx-auto">
                 <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid-cols-12">
                     {postData && postData.map((post, index)=>(
                     <article>
                         <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                        <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400" key={index}>
-                            <img src={post.mainImage.asset.url} className="w-full h-full rounded-r object-cover absolute" />
-                            <span className="block relative h-full flex justify-end items-end pr-4 bp-4">
-                                <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">{post.title}</h3>
+                        <span className="block mb-5 relative rounded shadow leading-snug bg-white border-l-8 border-green-400 hover:text-white-100 hover:bg-red-100" key={index}>
+                            {/* <img src={post.mainImage.asset.url} className="w-full h-full rounded-r object-cover absolute" /> */}
+                            <span className="block relative h-full flex justify-start items-end pr-4 bp-4">
+                                <h3 className="text-red-800 text-lg font-blog px-3 py-4 text-red-100 bg-opacity-75 rounded">{post.title}</h3>
                             </span>
                         </span>
                         </Link>
